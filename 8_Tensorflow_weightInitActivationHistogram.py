ebe5f8e8f85660c10e96adf13ec02bd7c02db68e
Soo Kim <77955283+zasminah@users.noreply.github.com>
2021-03-12 21:23:38 +0900

    Weight Init Activation Histogram
    Weight Init Activation Histogram

---
+++ 8_Tensorflow_weightInitActivationHistogram.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def ReLU(x):
+    return np.maximum(0, x)
+
+def tanh(x):
+    return np.tanh(x)
+
+input_data = np.random.randn(1000, 100)  # 1000개의 데이터
+node_num = 100  # 각 은닉층의 노드(뉴런) 수
+hidden_layer_size = 5  # 은닉층이 5개
+hidden_output = {}  # 이곳에 활성화 결과를 저장
+
+x = input_data
+
+for i in range(hidden_layer_size):
+    if i != 0:
+        x = hidden_output[i-1]
+    #w = np.random.randn(node_num, node_num) * 1
+    #w = np.random.randn(node_num, node_num) * 0.01
+    w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num) #xavier
+    #w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num) #he
+
+    ws = np.dot(x, w)
+
+    #hData = sigmoid(ws)
+    hData = ReLU(ws)
+    #hData = tanh(ws)
+
+    hidden_output[i] = hData
+
+# 히스토그램 그리기
+for i, a in hidden_output.items():
+    plt.subplot(1, len(hidden_output), i+1)
+    plt.title(str(i+1) + "-layer")
+    if i != 0:
+        plt.yticks([], [])
+    plt.xlim(0.1, 1)
+    plt.ylim(0, 7000)
+    plt.hist(a.flatten(), 30, range=(0, 1))
+
+plt.show()
